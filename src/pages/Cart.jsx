9a646495a0a5dfc98e39655d6b557e64469e8f05
Madi <maditop.12345@gmail.com>
2023-06-04 23:39:48 +0600

    cart start

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,47 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import cl from './pagesStyle/Cart.module.css'
+import axios from "axios";
 
 const Cart = () => {
+  const [goods, setGoods] = useState([])
+
+  async function getGoods(){
+    const response = await axios.get('http://localhost:3000/category-catalog')
+    setGoods(response.data)
+  }
+
+  useEffect(() => {
+    getGoods()
+  }, [])
+
   return (
-    <div>
-      
+    <div className={cl.main}>
+      <div className={cl.cartHeader}>
+
+      </div>
+      {goods
+        ?
+        goods.map((item, index) =>
+          <div key={item.id} className={cl.cartItem + ' row' }>
+            <div className={'col-1'}>
+              {index+1}
+            </div>
+            <div className={'col-2'}>
+              {item.name}
+            </div>
+            <div className={'col-3'}>
+              {item.price}
+            </div>
+            <div className={'col-4'}>
+              {item.count}
+            </div>
+            <div className='col-2'>
+              {item.price * item.count}
+            </div>
+          </div>
+        )
+        : ''
+      }
     </div>
   );
 };
--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,13 @@ import Home from "../pages/Home";
 import CategoryCatalog from "../pages/CategoryCatalog";
 import DoesNotExist from "../pages/DoesNotExist";
 import Product from "../pages/Product";
+import Cart from "../pages/Cart";
 
 
 export const routes = [
     {path: '/', component: <Home/>},
     {path: '/category-catalog/:category', component: <CategoryCatalog/>},
     {path: '/product/:product_id', component: <Product/>},
+    {path: '/cart', component: <Cart/>},
     {path: '*', component: <DoesNotExist/>},
 ]
